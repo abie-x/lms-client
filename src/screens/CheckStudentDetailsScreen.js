ee58068fd33540271ec4c3a036dd4c74c5889364
abie_x <abhiramzmenon@gmail.com>
2023-11-18 16:55:09 +0530

    added fee status button

--- src/screens/CheckStudentDetailsScreen.js
+++ src/screens/CheckStudentDetailsScreen.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 const CheckStudentDetailsScreen = () => {
 
+    // const [phoneNumber, setPhoneNumber]  = useState('')
+    const [name, setName] = useState('')
+    const [course, setCourse] = useState('')
+    const [batch, setBatch] = useState('')
+    const [year, setYear] = useState('')
+    const [error, setError] = useState(null)
+
+
     const editNumberHandler = () => {
-        console.log('iam editing something')
+        console.log('say hi')
     }
 
     const updateStudentHandler = () => {
-        console.log('iam updating something')
+        console.log(name)
+        console.log(course)
+        console.log(batch)
+        console.log(year)
     }
 
     const SearchComponent = () => {
+
+        const [phoneNumber, setPhoneNumber] = useState('')
+
+        const changeSearchQuery = (e) => {
+            console.log(e.target.value)
+            setPhoneNumber(e.target.value)
+        }
+
+        const editNumberHandler = () => {
+            setPhoneNumber('')
+        }
+
+        const updateStudentHandler = async () => {
+
+            const config = {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+            }
+
+            console.log('sending requests..')
+
+            const { data, message } = await axios.get(
+                `http://127.0.0.1:5000/api/students/details?phoneNumber=${phoneNumber}`,
+                config
+            )
+
+            if(data) {
+                setName(data.name)
+                setCourse(data.course)
+                setBatch(data.batch)
+                setYear(data.year)
+            }
+    
+        }
+
         return ( 
             <div className="mt-12">
-                <label for="search" class="block text-sm font-medium text-gray-900 mb-2">Enter mobile number of student</label>
+                <label for="phoneNumber" class="block text-sm font-medium text-gray-900 mb-2">Enter mobile number of student</label>
                 <div class="flex">
                     <div class="relative w-full md:w-full lg:w-1/2">
-                        <input type="search" id="search-dropdown" class="bg-white border border-gray-200 text-gray-900 text-sm rounded-xl block w-full p-2.5" placeholder="Search here.." required />
-                        <button type="submit" class="absolute top-0 end-0 p-3 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        <input type="text" name="phoneNumber" id="phoneNumber" className="bg-white border border-gray-200 text-gray-900 text-sm rounded-xl block w-full p-2.5 focus:ring-blue-500 focus:border-blue-500" placeholder="Search here.." onChange={(e) => changeSearchQuery(e)} required />
+                        <button class="absolute top-0 end-0 p-3 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300" onClick={() => updateStudentHandler()}>
                             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
@@ -34,20 +82,24 @@ const CheckStudentDetailsScreen = () => {
                 <div className="grid grid-cols-1 border border-gray-800 px-6 py-6 rounded-md">
                     <div className="flex justify-start text-sm mb-3">
                         <h4>Name: </h4>
-                        <h4 className="ml-4">Sreekumar</h4>
+                        <h4 className="ml-4">{name}</h4>
+                    </div>
+                    <div className="flex justify-start text-sm mb-3">
+                        <h4>Course: </h4>
+                        <h4 className="ml-4">{course}</h4>
                     </div>
                     <div className="flex justify-start text-sm mb-3">
                         <h4>Batch: </h4>
-                        <h4 className="ml-4">October</h4>
+                        <h4 className="ml-4">{batch}</h4>
                     </div>
                     <div className="flex justify-start text-sm mb-3">
                         <h4>Year: </h4>
-                        <h4 className="ml-4">2023 second</h4>
+                        <h4 className="ml-4">{year}</h4>
                     </div>
                     <h4 className="mt-12 text-xs text-gray-400">If you are seeing details of another student, click edit number</h4>
                     <div className="flex justify-center mt-8">
                         <button type="button" class="focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 font-medium rounded-3xl text-sm w-32 h-8 px-4  me-2 mb-2 transition">Edit</button>
-                        <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-4 font-medium rounded-3xl text-sm w-32 h-8 px-4 me-2 mb-2">Update</button>
+                        <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-4 font-medium rounded-3xl text-sm w-32 h-8 px-4 me-2 mb-2" onClick={updateStudentHandler}>Update</button>
                     </div>
                 </div>
             </div>
@@ -55,14 +107,14 @@ const CheckStudentDetailsScreen = () => {
     }
 
     return (
-        <div className="h-screen w-screen px-6 md:px-12 py-16 grid grid-cols-1 md:grid-cols-2">
+        <div className="h-screen w-screen px-6 md:px-20 pt-12 grid  grid-cols-1 md:grid-cols-2">
             <div className="200 pl-4">
                 <h2 className="text-lg md:text-xl lg:text-3xl font-semibold text-blue-500 mb-2">Enter Student Phone number</h2>
                 <h4 className="text-sm md:text-md lg:text-lg font-semibold text-gray-400">& check if details are correct</h4>
                 <SearchComponent />
                 <CardComponent />
             </div>
-            <div className=" max-[640px]:hidden mt-24 lg:-mt-7 md:w-full lg:w-4/5">
+            <div className=" max-[640px]:hidden mt-24 lg:-mt-0 md:w-full lg:w-5/6">
                 <img src="./phone_img.png" />
             </div>
         </div>
--- src/screens/NavigationScreen.js
+++ src/screens/NavigationScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useLocation, useNavigate} from 'react-router-dom'
+import { MdPayment } from "react-icons/md";
 
 const NavigationScreen = () => {
     const { state } = useLocation();
@@ -18,8 +19,27 @@ const NavigationScreen = () => {
 
     return (
         <div className="w-screen h-screen px-8 pt-12 pb-20 sm:px-16 sm:py-32">
-            <h1 className="text-blue-500 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold lg:font-semibold mb-4">Hello {name}👋</h1>
-            <h1 className="text-lg">Choose what you wish to do from below</h1>
+            <div className="grid grid-cols-4">
+                <div className="col-span-3">
+                    <h1 className="text-blue-500 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold lg:font-semibold mb-4">Hello {name}👋</h1>
+                    <h1 className="text-md md:text-lg">Choose what you wish to do</h1>
+                </div>
+                {/* <button type="button" class="w-20 h-20 border border-blue-300 text-blue-600 bg-slate-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full  text-sm ">
+                    <div className="font-semibold">Fee</div>
+                    <div className="text-xs">Update</div>
+                </button> */}
+                <div className="flex justify-center items-center md:hidden">
+                    <button type="button" class="w-16 h-16 border border-blue-300 text-blue-600 bg-slate-300 hover:bg-blue-800 focus:ring-blue-300 rounded-full p-2  text-sm">
+                        <div className="font-semibold">Fee</div>
+                        <div className="text-xs">Update</div>
+                    </button>
+                </div>
+                <div className="hidden md:flex justify-end items-center">
+                    <button type="button" class="flex text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-2 py-2.5 mb-2"><MdPayment className="mt-0 mr-2 h-5 w-5 md:h-5 md:w-5" /> Update fee</button>
+                </div>
+
+
+            </div>
             <div className="mt-8 w-full  md:grid md:grid-cols-3 gap-4 ">
                 <div class="border border-blue-400 rounded-xl p-4 mb-4 md:col-span-1 md:h-64 lg:h-80 lg:flex lg:justify-center lg:items-center cursor-pointer" onClick={navigateAddStudentsScreen}>
                     <div className="grid grid-cols-3 md:block">
