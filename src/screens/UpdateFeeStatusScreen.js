92ccb29be7c38d6613c0d830455da00e51acb406
abie_x <abhiramzmenon@gmail.com>
2023-11-22 17:15:30 +0530

    testing..

--- src/screens/UpdateFeeStatusScreen.js
+++ src/screens/UpdateFeeStatusScreen.js
@@ -1,21 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Select from 'react-select';
+import axios from "axios";
 
 const UpdateFeeStatusScreen = () => {
 
     const [feeType, setFeeType] = useState(null)
     const [paymentType, setPaymentType] = useState(null)
     const [isFocused, setIsFocused] = useState(true);
-    const [amount, setAmount] = useState('')
+    const [studentDetails, setStudentDetails] = useState(null)
+    const [amount, setAmount] = useState(0)
+
+    const [student, setStudent] = useState(null)
 
     const feeOptions = [
         {
             label: 'Registration fee',
-            value: 'registrationFee'
+            value: 'registrationFees'
         },
         {
             label: 'Exam fee',
-            value: 'examFee'
+            value: 'examFees'
         }, 
         {
             label: 'First term fee',
@@ -42,56 +46,104 @@ const UpdateFeeStatusScreen = () => {
         }
     ]
 
+    //getting the student details
+    useEffect(() => {
+        const getStudentData = async () => {
+            const {data} = await axios.get('http://localhost:5000/api/students/details?phoneNumber=9745759884')
+            console.log('sending data...')
+            console.log(data)
+            if(data) {
+                setStudent(data)
+            }
+        }
+        getStudentData()
+    }, [])
+
+    const updateFeeHandler = async () => {
+        const config = {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        }
+
+        console.log('sending requests..')
+
+        let installmentNumber
+        console.log(paymentType)
+        // paymentType === 'firstTerm' ? installmentNumber = 1 : paymentType === 'secondTerm' ? installmentNumber = 2 : paymentType === 'thirdTerm' ? installmentNumber = 3 : installmentNumber = 0
+        if(feeType === 'firstTerm') {
+            installmentNumber = 1
+        } else if(feeType === 'secondTerm') {
+            installmentNumber = 2
+        } else if(feeType === 'thirdTerm') {
+            installmentNumber = 3
+        } else {
+            console.log('something went wrong!')
+            installmentNumber = 4
+        }
+
+        const { data } = await axios.put(
+            'http://127.0.0.1:5000/api/students/fees/nios',
+            { phoneNumber: '9745759884', feeType, installmentNumber, amount: parseInt(amount) },
+            config
+        )
+
+        console.log('iam receiving requests')
+        console.log(data)
+    }
+
     return (
         <div className="h-fit w-screen bg-slate-100 px-6 md:px-20 py-8 lg:grid lg:grid-cols-2">
             <div className="h-full w-full md:px-28 lg:px-4 xl:px-28 xl:mt-2 md:py-3 bg-slate-100" >
                 <h3 className="text-base md:text-xl font-semibold">Student details</h3>
                 <div className="mt-5 text-sm flex gap-4">
                     <h4 className="text-sm md:text-lg">Name: </h4>
-                    <h4 className="text-sm md:text-lg">Sreekumar R</h4>
+                    <h4 className="text-sm md:text-lg">{student && student.name}</h4>
                 </div>
                 <div className="mt-2 text-sm flex gap-4">
                     <h4 className="text-sm md:text-lg">Batch: </h4>
-                    <h4 className="text-sm md:text-lg">October 2022</h4>
+                    <h4 className="text-sm md:text-lg">{student && `${student.intake} ${student.year}`}</h4>
                 </div>
                 <div className="w-full h-36 mt-6 bg-slate-200 rounded-2xl p-5">
                     <h3 className="text-sm font-semibold">Past payment status</h3>
                     <div className="grid grid-cols-5 gap-2 mt-4">
                         <div className="w-full me-2 mb-2">
-                            <button type="button" class="w-full text-green-500 border border-green-400 font-medium rounded-lg text-sm px-1 py-1">
+                            <button type="button" className={`w-full ${student && student.feeDetails.examFeePaid ? 'text-green-500' : 'text-red-500'}  border ${student && student.feeDetails.examFeePaid ? 'border-green-500' : 'border-red-500'} font-medium rounded-lg text-sm px-1 py-1`}>
                                 <div>Reg</div>
                                 <div>Fee</div>
                             </button>
                         </div>
                         <div className="w-full me-2 mb-2">
-                            <button type="button" class="w-full text-green-500 border border-green-400 font-medium rounded-lg text-sm px-1 py-1">
+                            <button type="button" className= {`w-full ${student && student.feeDetails.registrationFeePaid ? 'text-green-500' : 'text-red-500'} border ${student && student.feeDetails.registrationFeePaid ? 'border-green-500' : 'border-red-500'} font-medium rounded-lg text-sm px-1 py-1`}>
                                 <div>Exam</div>
                                 <div>Fee</div>
                             </button>
                         </div>
                         <div className="w-full me-2 mb-2">
-                            <button type="button" class="w-full text-red-500 border border-red-400 font-medium rounded-lg text-sm px-1 py-1">
+                            <button type="button" className={`w-full ${student && student.feeDetails.installments[0].isPaid ? 'text-green-500' : 'text-red-500'} border ${student && student.feeDetails.installments[0].isPaid ? 'border-green-500' : 'border-red-500'} font-medium rounded-lg text-sm px-1 py-1`}>
                                 <div>First</div>
                                 <div>Term</div>
                             </button>
-                            <label for="button" class="block text-xs font-medium text-red-600 mt-2 text-center">1000 bal</label>
+                            <label for="button" class={`${student && student.feeDetails.installments[0].isPaid ? 'hidden' : 'block'} text-xs font-medium text-red-600 mt-2 text-center`}>{student && student.feeDetails.installments[0].paidAmount > 0 && `${student.feeDetails.installments[0].amount - student.feeDetails.installments[0].paidAmount} bal`}</label>
                         </div>
                         <div className="w-full me-2 mb-2">
-                            <button type="button" class="w-full text-red-500 border border-red-400 font-medium rounded-lg text-sm px-1 py-1">
+                            <button type="button" className={`w-full ${student && student.feeDetails.installments[1].isPaid ? 'text-green-500' : 'text-red-500'} border ${student && student.feeDetails.installments[1].isPaid ? 'border-green-500' : 'border-red-500'} font-medium rounded-lg text-sm px-1 py-1`}>
                                 <div>Sec</div>
                                 <div>Term</div>
                             </button>
+                            <label for="button" class={`${student && student.feeDetails.installments[1].isPaid ? 'hidden' : 'block'} text-xs font-medium text-red-600 mt-2 text-center`}>{student && student.feeDetails.installments[1].paidAmount > 0 && `${student.feeDetails.installments[1].amount - student.feeDetails.installments[1].paidAmount} bal`}</label>
                         </div>
                         <div className="w-full me-2 mb-2">
-                            <button type="button" class="w-full text-red-500 border border-red-400 font-medium rounded-lg text-sm px-1 py-1">
-                                <div>Third</div>
+                            <button type="button" className={`w-full ${student && student.feeDetails.installments[2].isPaid ? 'text-green-500' : 'text-red-500'} border ${student && student.feeDetails.installments[2].isPaid ? 'border-green-500' : 'border-red-500'} font-medium rounded-lg text-sm px-1 py-1`}>
+                                <div>Sec</div>
                                 <div>Term</div>
                             </button>
+                            <label for="button" class={`${student && student.feeDetails.installments[2].isPaid ? 'hidden' : 'block'} text-xs font-medium text-red-600 mt-2 text-center`}>{student && student.feeDetails.installments[2].paidAmount > 0 && `${student.feeDetails.installments[2].amount - student.feeDetails.installments[2].paidAmount} bal`}</label>
                         </div>
                     </div>
                 </div>
                 <div class="mb-3 mt-6 px-3">
-                        <label for="feeType" class="block text-sm font-medium text-gray-900 mb-2">Select fee type</label>
+                        <label for="feeType" class="block text-sm font-medium text-gray-900 mb-2">Select payment type</label>
                         <Select options={feeOptions} styles={{
                             control: (baseStyles, state) => ({
                             ...baseStyles,
@@ -117,10 +169,10 @@ const UpdateFeeStatusScreen = () => {
                     <input type="text" id="enrollmentNumber" class="bg-white border border-white text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="CDJ1233J" onChange={(e) => setAmount(e.target.value)} required/>
                 </div>
                 <div className="w-full px-3 flex justify-end">
-                    <label className="text-xs md:text-sm font-medium text-gray-900 mb-2">Total fee: 3500 <span className="ml-5 text-red-500">Pending fee: 5000</span></label>
+                    <label className="text-xs md:text-sm font-medium text-gray-900 mb-2">Total fee: {student && student.feeDetails.totalAmount} <span className="ml-5 text-red-500">Pending fee: {student && student.feeDetails.totalAmount - student.feeDetails.paidAmount}</span></label>
                 </div>
                 <div className="w-full flex justify-end mt-2 mb-1">
-                    <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-red-800 focus:ring-4 font-medium rounded-md text-sm px-2 py-2 md:px-5 md:py-2.5 transition">Update fee</button>
+                    <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-red-800 focus:ring-4 font-medium rounded-md text-sm px-2 py-2 md:px-5 md:py-2.5 transition" onClick={updateFeeHandler}>Update fee</button>
                 </div>
             </div> 
             <div className="hidden lg:block h-full w-full bg-red-300">
