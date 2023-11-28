65abef8229fb71e66c91719d52d102f2d400f3b2
abie_x <abhiramzmenon@gmail.com>
2023-11-28 11:52:52 +0530

    testing the student modification api

--- src/screens/StudentModificationScreen.js
+++ src/screens/StudentModificationScreen.js
@@ -146,7 +146,7 @@ const StudentModificationScreen = () => {
         }
 
         const { data } = await axios.put(
-            `http://127.0.0.1:5000/api/students/updateExisting/${id}`,
+            `https://jellyfish-app-wmpnc.ondigitalocean.app/api/students/updateExisting/${id}`,
             requestBody,
             config
         )
